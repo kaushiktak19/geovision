5bcc2845b2d6119a8ee9af1095645bf0b339a2d3
kaushiktak19 <kaushiktak19@gmail.com>
2024-07-07 20:02:38 +0530

    login page

---
+++ src/components/LoginPage.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import '../styles/LoginPage.css';
+
+const LoginPage: React.FC = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [showCredentials, setShowCredentials] = useState(false);
+
+    const handleLogin = () => {
+        if ((username === 'user@gmail.com' || username === 'user')  && password === 'password') {
+            window.location.href = '/map'; // Navigate to '/map' on successful login
+        } else {
+            alert('Invalid username or password');
+        }
+    };
+
+    return (
+        <div className="login-container">
+            <div className="login-box">
+                <h2>Login</h2>
+                <form>
+                    <div className="form-group">
+                        <label>Username:</label>
+                        <input
+                            type="text"
+                            placeholder="Enter username or email"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Password:</label>
+                        <input
+                            type="password"
+                            placeholder="Enter password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <button type="button" onClick={handleLogin}>Login</button>
+                    </div>
+                    <div>
+                        <button type="button" onClick={() => setShowCredentials(!showCredentials)}>
+                            {showCredentials ? 'Hide' : 'Show'} Login Credentials
+                        </button>
+                        {showCredentials && (
+                            <div className="credentials">
+                                <p>Username: user</p>
+                                <p>Email: user@gmail.com</p>
+                                <p>Password: password</p>
+                            </div>
+                            )}
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default LoginPage;
